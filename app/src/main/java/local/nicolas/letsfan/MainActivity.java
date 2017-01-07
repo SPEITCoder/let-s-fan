e657432f61fc82012412c859e0d1619e5b7c1023
luckyxi94 <luckyxi94@gmail.com>
2017-01-07 17:46:55 +0800

    update layout.app_bar_main

--- app/src/main/java/local/nicolas/letsfan/MainActivity.java
+++ app/src/main/java/local/nicolas/letsfan/MainActivity.java
@@ -137,9 +137,10 @@ public class MainActivity extends AppCompatActivity
             @Override
             protected void populateViewHolder(InvitationViewHolder viewHolder, Invitation invitation, int position) {
                 //mProgressBar.setVisibility(ProgressBar.INVISIBLE);
-                viewHolder.organizerTextView.setText(invitation.getOrganizer());
-                //viewHolder.organizerTextView.setText("test");
-                viewHolder.dateTextView.setText(invitation.getDateDay().toString());
+                String mDate=invitation.getDateMonth().toString()+"."+invitation.getDateDay();
+                viewHolder.organizerTextView.setText(invitation.getOrganizerNickName());
+                viewHolder.dateTextView.setText(mDate);
+
             }
         };
 
